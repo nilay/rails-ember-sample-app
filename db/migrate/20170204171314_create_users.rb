9fe866c1320ec4c7167dae783a05ee46a96754d8
Nilay <nilayanand@gmail.com>
2017-02-04 22:56:36 +0530

    Add user model

---
+++ db/migrate/20170204171314_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password_digest
+      t.string :password_reset_token
+      t.datetime :password_reset_sent_at
+
+      t.timestamps
+    end
+
+    add_index :users, :email, unique: true
+  end
+end
